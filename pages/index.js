b559c758f46c593f8d2fd57ae848fe4edb1a978e
pixelavii <pixelavii007@gmail.com>
2025-04-11 20:05:44 +0530

    Header Added

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import DevDash from "@/components/DevDash";
 import * as React from "react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import Head from "next/head";
 
 export default function Home() {
   const router = useRouter();
@@ -17,7 +18,23 @@ export default function Home() {
       router.push("/devs");
     }
   }, []);
-  if (!token) return <div>Loading...</div>
+  if (!token) return <div className="text-white bg-black md:text-5xl text-xl flex justify-center items-center h-[100vh] w-full">Loading...</div>
 
-  return <DevDash />;
+  return (
+    <>
+      <Head>
+        <title>Devs Port – Developer’s Digital Identity</title>
+        <meta name="description" content="Consolidate your online presence into one powerful developer profile. Showcase GitHub, LinkedIn, LeetCode, and more with Devs Port." />
+        <meta name="keywords" content="Devs Port, Developer Portfolio, Create Easy Developer's Profile" />
+        <meta name="author" content="Avinash" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Devs Port – Developer’s Digital Identity" />
+        <meta property="og:description" content="Build and share your complete developer portfolio with a single link. One profile. Every platform." />
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:url" content="https://devsport.vercel.app/" />
+      </Head>
+      <DevDash />
+    </>
+
+  )
 }
